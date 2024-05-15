c820d537e83a82620e744a5138d301559b122a69
mooncake <adom.donatas@gmail.com>
2024-05-15 18:45:43 +0300

    Move inbank logo to a better location

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -3,9 +3,9 @@ import { RouterLink } from 'vue-router';
 </script>
 
 <template>
-  <div class="m-4 xl:mx-10 ml:py-6">
+  <div class="m-4 xl:mx-10">
     <RouterLink to="/">
-      <img alt="Inbank logo" src="@/assets/inbank_logo.svg" class="w-28 h-auto xl:w-44" />
+      <img alt="Inbank logo" src="@/assets/images/inbank_logo.svg" class="w-28 h-auto xl:w-44" />
     </RouterLink>
   </div>
 </template>
